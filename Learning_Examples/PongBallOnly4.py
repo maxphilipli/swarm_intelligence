eb9e789a9e67b693eb165618833bdb2fd07e6e67
cgpollock2 <cgpollock2@gmail.com>
2016-03-09 20:54:39 -0500

    Created Bot and Swarm classes

--- Learning_Examples/PongBallOnly4.py
+++ Learning_Examples/PongBallOnly4.py
@@ -12,9 +12,7 @@ class View(QGraphicsView,QObject):
         self.viewport=QGLWidget()
         self.setViewport(self.viewport)
         self.setFixedSize(700,400)
-        self.centerOn(0,0)
-        # self.setAlignment(Qt.AlignTop)
-        # self.setAlignment(Qt.AlignLeft)        
+        self.setAlignment(Qt.AlignTop|Qt.AlignLeft)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setWindowTitle('Pong')
@@ -24,21 +22,21 @@ class View(QGraphicsView,QObject):
         #self.setSceneRect(self.x()-2,self.y()+self.height()/2-2,self.width(),self.height())
         #self.centerOn(0,0)
 
-    # Wpress=pyqtSignal()
-    # Spress=pyqtSignal()
-    # UPpress=pyqtSignal()
-    # DownPress=pyqtSignal()
+    Wpress=pyqtSignal()
+    Spress=pyqtSignal()
+    UPpress=pyqtSignal()
+    DownPress=pyqtSignal()
 
-    # def keyPressEvent(self,event):
-    #    # print chr(event.key())
-    #     if event.key()==Qt.Key_W:
-    #         self.Wpress.emit()
-    #     elif event.key()==Qt.Key_S:
-    #         self.Spress.emit()
-    #     elif event.key()==Qt.Key_Up:
-    #         self.UPpress.emit()
-    #     elif event.key()==Qt.Key_Down:
-    #         self.DownPress.emit()
+    def keyPressEvent(self,event):
+       # print chr(event.key())
+        if event.key()==Qt.Key_W:
+            self.Wpress.emit()
+        elif event.key()==Qt.Key_S:
+            self.Spress.emit()
+        elif event.key()==Qt.Key_Up:
+            self.UPpress.emit()
+        elif event.key()==Qt.Key_Down:
+            self.DownPress.emit()
         
         
     
@@ -78,20 +76,20 @@ class Ball(QGraphicsEllipseItem):
 
 
 
-# class Paddle(QGraphicsRectItem):
-#     def __init__(self,maxHeight,parent=None):
-#         super(Paddle,self).__init__(parent)
-#         self.maxHeight=maxHeight-105
+class Paddle(QGraphicsRectItem):
+    def __init__(self,maxHeight,parent=None):
+        super(Paddle,self).__init__(parent)
+        self.maxHeight=maxHeight-105
 
-#     def moveUp(self):
-#         #print self.y(),self.maxHeight
-#         if self.y()>0:
-#             self.setY(self.y()-10)
+    def moveUp(self):
+        #print self.y(),self.maxHeight
+        if self.y()>0:
+            self.setY(self.y()-10)
 
-#     def moveDown(self):
-#         #print self.y(),self.maxHeight
-#         if self.y()<self.maxHeight:
-#             self.setY(self.y()+10)
+    def moveDown(self):
+        #print self.y(),self.maxHeight
+        if self.y()<self.maxHeight:
+            self.setY(self.y()+10)
 
 
 
@@ -100,7 +98,7 @@ class PongGame:
         self.sceneView=SceneAndView()
         self.pen=QPen(Qt.green,5,Qt.SolidLine,Qt.FlatCap,Qt.RoundJoin)
         self.ballbrush=QBrush(Qt.red)
-        # self.paddlebrush=QBrush(Qt.black)
+        self.paddlebrush=QBrush(Qt.black)
         
         self.ball=Ball()
         self.ball.setBrush(self.ballbrush)
@@ -109,14 +107,13 @@ class PongGame:
         #print self.sceneView.view.geometry()
         self.ball.setRect((self.sceneView.view.size().width()-15)/2,self.sceneView.view.height(),self.ball.radius,self.ball.radius)
         self.boundary=[self.sceneView.view.width(),self.sceneView.view.height()]
-        #self.ball.
-        # self.paddleLeft=Paddle(self.sceneView.view.height())
-        # self.paddleLeft.setRect(0,self.sceneView.view.size().height()/2,10,100)
-        # self.paddleRight=Paddle(self.sceneView.view.height())
-        # self.paddleRight.setRect(self.sceneView.view.size().width()-15,self.sceneView.view.size().height()/2,10,100)
-        # self.score=[0,0]
-        # self.sceneView.scene.addLine(100,200,100,100)
-        # self.sceneView.scene.addLine(self.boundary[0]/2,0,self.boundary[0]/2,self.boundary[1]*2)
+        # #self.ball.
+        self.paddleLeft=Paddle(self.sceneView.view.height())
+        self.paddleLeft.setRect(0,self.sceneView.view.size().height()/2,10,100)
+        self.paddleRight=Paddle(self.sceneView.view.height())
+        self.paddleRight.setRect(self.sceneView.view.size().width()-15,self.sceneView.view.size().height()/2,10,100)
+        self.score=[0,0]
+        self.sceneView.scene.addLine(self.boundary[0]/2,self.boundary[1]/2,self.boundary[0]/2,self.boundary[1]*2)
         # self.paddleLeft.setBrush(self.paddlebrush)
         # self.paddleRight.setBrush(self.paddlebrush)
         #self.sceneView.view.setSceneRect(150,150,900,600)
@@ -133,9 +130,8 @@ class PongGame:
         
         self.sceneView.scene.addItem(self.scoreText)
         
-        # self.updateScore()
+        self.updateScore()
         self.scoreText.setPos(self.boundary[0]/2-self.scoreText.boundingRect().width()*.85,self.boundary[1]/2)
-        # self.scoreText.setPos(1400,800)
         self.scoreText.setFont(QFont('Arial',15))
         
         self.timer=QTimer(self.sceneView.view)
@@ -143,14 +139,15 @@ class PongGame:
         self.reset()
 
     
-    # def updateScore(self):
-    #     message=str(self.score[0])+'    '+str(self.score[1])
-    #     self.scoreText.setPlainText(message)                                         
+    def updateScore(self):
+        message=str(self.score[0])+'    '+str(self.score[1])
+        self.scoreText.setPlainText(message)                                         
         
         
-        # self.sceneView.view.centerOn(0,0)
+        #self.sceneView.view.centerOn(0,0)
+        
     def reset(self):
-        # self.score=[0,0]
+        self.score=[0,0]
         self.serve(random.choice([0,1]))
 
     def serve(self,leftSide):
@@ -158,19 +155,13 @@ class PongGame:
         #print self.ball.pos().x(),self.ball.y(),'x and y'
         #print leftSide
         self.ball.setPos(0,0)
-        # self.sceneView.view.centerOn(0,0)
         #self.sceneView.view.centerOn(0,0)
         # if leftSide:
         #     self.ball.vel=[-10,random.choice([-3,-2,-1,1,2,3])]
         # else:
         #     self.ball.vel=[10,random.choice([-3,-2,-1,3,2,1])]
-
-        # if leftSide:
-        #     self.ball.vel=[0,0]
-        # else:
-        #     self.ball.vel=[0,0]
         #self.ball.vel=[1,random.choice([-3,-2,-1,1,2,3])]
-        self.timer.start(30)
+        self.timer.start(32)
 
     def ballMove(self):
         #print self.ball.y(),self.boundary[1]
@@ -178,18 +169,18 @@ class PongGame:
 ##            print 'leftpaddlecollide'
         if self.ballHitsBoundary():
             self.ball.reflectY()
-        elif self.ballHitsWalls():
-            self.ball.reflectX()
+        # elif self.ballHitsPaddle():
+        #     self.ball.reflectX()
             #self.ball.reflectY()
-        # elif self.ballMissed()[0]:
-        #     if self.ballMissed()[1]=='left':
-        #         self.score[1]+=1
-        #         self.serve(True)
-        #     elif self.ballMissed()[1]=='right':
-        #         self.score[0]+=1
-        #         self.serve(False)
-            # self.sceneView.view.centerOn(0,0)
-            # self.updateScore()
+        elif self.ballMissed()[0]:
+            if self.ballMissed()[1]=='left':
+                self.score[1]+=1
+                self.serve(True)
+            elif self.ballMissed()[1]=='right':
+                self.score[0]+=1
+                self.serve(False)
+            #self.sceneView.view.centerOn(0,0)
+            self.updateScore()
             #self.serve(s)
             
 
@@ -198,38 +189,27 @@ class PongGame:
 
     
     
-    # def ballHitsPaddle(self):
-    #     z= self.sceneView.scene.collidingItems(self.ball)
-    #     if self.ball.collidesWithItem(self.paddleLeft) or self.ball.collidesWithItem(self.paddleRight):
-    #         return True
-    #     pass
+    def ballHitsPaddle(self):
+        z= self.sceneView.scene.collidingItems(self.ball)
+        if self.ball.collidesWithItem(self.paddleLeft) or self.ball.collidesWithItem(self.paddleRight):
+            return True
+        pass
             
-    # def ballHitsBoundary(self):
-    #     if self.ball.y()<-self.boundary[1]/2+15 or self.ball.y()>self.boundary[1]/2-15:
-    #         return True
-    #     pass
-
     def ballHitsBoundary(self):
-        if self.ball.y()<- 0 or self.ball.y()> 400:
+        if self.ball.y()<-self.boundary[1]/2+15 or self.ball.y()>self.boundary[1]/2-15:
             return True
         pass
 
-    def ballHitsWalls(self):
-        z = self.sceneView.scene.collidingItems(self.ball)
-        if self.ball.x() < 0 or self.ball.x() > 700:
-            return True
-        pass
-
-    # def ballMissed(self):
-    #     #print self.boundary[0]/2,self.ball.x()
-    #     if self.ball.x()<-self.boundary[0]/2:
-    #         #print 'missed left'
-    #         return True,'left'
-    #     elif self.ball.x()>self.boundary[0]/2:
-    #         #print 'missed right'
-    #         return True,'right'
-    #     else:
-    #         return False,'none'
+    def ballMissed(self):
+        #print self.boundary[0]/2,self.ball.x()
+        if self.ball.x()<-self.boundary[0]/2:
+            #print 'missed left'
+            return True,'left'
+        elif self.ball.x()>self.boundary[0]/2:
+            #print 'missed right'
+            return True,'right'
+        else:
+            return False,'none'
 
 
 
---
+++ Swarm.py
@@ -0,0 +1,58 @@
+import math
+
+
+class Swarm():
+
+	def __init__(self, xPos, yPos, velocity, theta):
+		self.xPos = xPos
+		self.yPos = yPos
+		self.velocity = velocity
+		self.theta = theta
+		self.size = 1
+		self.sight = 10
+
+	
+
+	def calcPos(self):
+		# Calculates the position based on velocity and theta
+		print("calcPos")
+		self.xPos = self.xPos + self.velocity*math.cos(self.theta)
+		self.yPos = self.yPos + self.velocity*math.sin(self.theta)
+
+
+
+
+	def calcVelocity(self):
+		# Calculates the velocity for the bot
+		print("calcVelocity")
+		self.velocity  += 1
+		
+
+	def calcTheta(self):
+		#Calculates the theta for the bot
+		print("calcTheta")
+
+
+
+
+if __name__=="__main__":
+	a = Bot(0,0,0,math.pi/4)
+
+	print("xpos = ", a.xPos)
+	print("ypos = ", a.yPos)
+	print("theta = ", a.theta)
+	print("sight = ", round(a.sight))
+
+	a.calcVelocity()
+	a.calcPos()
+
+	print("xpos = ", a.xPos)
+	print("ypos = ", a.yPos)
+	print("vel = ", a.velocity)
+
+	a.calcVelocity()
+	a.calcPos()
+
+	print("xpos = ", a.xPos)
+	print("ypos = ", a.yPos)
+	print("vel = ", a.velocity)

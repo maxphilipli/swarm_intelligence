0a07acfbece1f6a3a01492aa1e092c9ec95cdaa8
cgpollock2 <cgpollock2@gmail.com>
2016-03-09 21:17:24 -0500

    Update Swarm
    Initial swarm definition. Previous version was same as Bot

--- Swarm.py
+++ Swarm.py
@@ -1,58 +1,53 @@
 import math
+import random
+import Bot
 
 
 class Swarm():
 
-	def __init__(self, xPos, yPos, velocity, theta):
-		self.xPos = xPos
-		self.yPos = yPos
-		self.velocity = velocity
-		self.theta = theta
-		self.size = 1
-		self.sight = 10
+	def __init__(self, numberBots, startSize):
+		self.numberBots = numberBots
+		self.startSize = startSize
+		self.botList = []
 
-	
+		for i in range(0,numberBots):
+			# create random position of bot within defined radius
+			radiusSwarm = random.uniform(0, self.startSize)
+			thetaSwarm = random.uniform(0, 2*math.pi)
 
-	def calcPos(self):
-		# Calculates the position based on velocity and theta
-		print("calcPos")
-		self.xPos = self.xPos + self.velocity*math.cos(self.theta)
-		self.yPos = self.yPos + self.velocity*math.sin(self.theta)
+			# define Bot initial variables
+			xPosBot = radiusSwarm * math.cos(thetaSwarm)
+			yPosBot = radiusSwarm * math.sin(thetaSwarm)
+			thetaBot = 0
+			velocityBot = 0
 
+			# create Bot and add to list
+			newBot = Bot.Bot(xPosBot,yPosBot, velocityBot, thetaBot)
+			self.botList.append(newBot)
 
-
-
-	def calcVelocity(self):
-		# Calculates the velocity for the bot
-		print("calcVelocity")
-		self.velocity  += 1
-		
-
-	def calcTheta(self):
-		#Calculates the theta for the bot
-		print("calcTheta")
 
 
 
 
 if __name__=="__main__":
-	a = Bot(0,0,0,math.pi/4)
+	print("its alive")
+	# a = Bot(0,0,0,math.pi/4)
 
-	print("xpos = ", a.xPos)
-	print("ypos = ", a.yPos)
-	print("theta = ", a.theta)
-	print("sight = ", round(a.sight))
+	# print("xpos = ", a.xPos)
+	# print("ypos = ", a.yPos)
+	# print("theta = ", a.theta)
+	# print("sight = ", round(a.sight))
 
-	a.calcVelocity()
-	a.calcPos()
+	# a.calcVelocity()
+	# a.calcPos()
 
-	print("xpos = ", a.xPos)
-	print("ypos = ", a.yPos)
-	print("vel = ", a.velocity)
+	# print("xpos = ", a.xPos)
+	# print("ypos = ", a.yPos)
+	# print("vel = ", a.velocity)
 
-	a.calcVelocity()
-	a.calcPos()
+	# a.calcVelocity()
+	# a.calcPos()
 
-	print("xpos = ", a.xPos)
-	print("ypos = ", a.yPos)
-	print("vel = ", a.velocity)
+	# print("xpos = ", a.xPos)
+	# print("ypos = ", a.yPos)
+	# print("vel = ", a.velocity)

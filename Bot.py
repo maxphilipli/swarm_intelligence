7a9798487cd041a659f1b9cff27db7bbcd52237e
Max <maxphilipli@gmail.com>
2016-03-21 20:57:13 -0400

    Velocity and convergence

--- Bot.py
+++ Bot.py
@@ -8,45 +8,63 @@ class Bot():
 		self.yPos = yPos
 		self.velocity = velocity
 		self.theta = theta
-		self.size = 1
+		self.size = 5
 		self.sight = 10
 
 	
 
 	def calcPos(self):
 		# Calculates the position based on velocity and theta
-		print("calcPos")
 		self.xPos = self.xPos + self.velocity*math.cos(self.theta)
 		self.yPos = self.yPos + self.velocity*math.sin(self.theta)
 
 
+	def distance(self, x, y, x2, y2):
+		return math.sqrt((x-x2)*(x-x2) + (y-y2)*(y-y2))
 
-
-	def calcVelocity(self):
+	def calcVelocity(self, x, y):
 		# Calculates the velocity for the bot
-		print("calcVelocity")
-		self.velocity  += 1
-		
+		factor = 1
+		dist = self.distance(self.xPos, self.yPos, x, y)
+		# print (dist)
+		self.velocity = factor * dist
+		if self.velocity >= 4:
+			self.velocity = 4
+		if dist <= 5:
+			self.velocity = 0
+		self.theta = math.atan2(y - self.yPos, x - self.xPos)
 
-	def calcTheta(self, something):
-		#Calculates the theta for the bot
-		print("calcTheta")
+	def move(self, x, y):
+		# Function to simply movement function call in GUI
+		self.calcVelocity(x, y)
+		self.calcPos()
+
+
+
 
 
 
 
 if __name__=="__main__":
-	a = Bot(0,0,0,math.pi/4)
+	a = Bot(0,0,0,math.pi)
 
 	print("xpos = ", a.xPos)
 	print("ypos = ", a.yPos)
 	print("theta = ", a.theta)
 	print("sight = ", round(a.sight))
 
-	for  i in range(0, 10):
-		a.calcVelocity()
-		a.calcPos()
+	a.move(1, 1)
 
-		print("xpos = ", a.xPos)
-		print("ypos = ", a.yPos)
-		print("vel = ", a.velocity)
+	print ("")
+	print("xpos = ", a.xPos)
+	print("ypos = ", a.yPos)
+	print("theta = ", a.theta)
+	print("sight = ", round(a.sight))
+
+	# for  i in range(0, 10):
+	# 	a.calcVelocity()
+	# 	a.calcPos()
+
+	# 	print("xpos = ", a.xPos)
+	# 	print("ypos = ", a.yPos)
+	# 	print("vel = ", a.velocity)
---
+++ SI.py
@@ -0,0 +1,91 @@
+import sys
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import time
+import math
+import Bot
+import Swarm
+
+
+#-----------------------------------------------------------#
+#GUI class definition
+class Window(QtGui.QMainWindow):
+
+	#---------------------------------------------#
+	#define class variables
+	msPtX = 20
+	msPtY = 20
+
+	swarm = Swarm.Swarm(100, 20)
+	#---------------------------------------------#
+	#define what to do on initialization
+	def __init__(self):
+		super(Window, self).__init__()
+		self.setGeometry(0, 0, 500, 500)
+		self.setWindowTitle("Final Form")
+		self.timer = QtCore.QTimer()
+		self.timer.timeout.connect(self.update)
+		self.timer.start(20)
+		self.show()
+
+	#---------------------------------------------#
+	#grab coordinates of mouse click
+	def mousePressEvent(self, QMouseEvent):
+		mousePoint = (str(QMouseEvent.pos())[20:])
+		mousePoint = mousePoint.rsplit(", ")
+		msPtX = mousePoint[0]
+		noEndP = mousePoint[1].rsplit(")")
+		msPtY = noEndP[0]
+		self.msPtX = int(msPtX)
+		self.msPtY = int(msPtY)
+
+	#---------------------------------------------#
+	#movement to mouse coordinates after mouse click
+	def moveToMouse(self):
+		for element in self.swarm.botList:
+			element.move(self.msPtX, self.msPtY)
+
+
+	#---------------------------------------------#
+	#paint event being looped to simulate animation
+	def paintEvent(self, event):
+		paint = QPainter()
+		paint.begin(self)
+		paint.setRenderHint(QPainter.Antialiasing)
+		#set background
+		paint.setBrush(Qt.white)
+		paint.drawRect(event.rect())
+		#set circle
+		paint.setPen(Qt.black)
+		paint.setBrush(Qt.green)
+		#draw circle
+		# rad = 15
+		# center = QPoint(self.bot.xPos, self.bot.yPos)
+		# paint.drawEllipse(center, rad, rad)
+
+		#draw a swarm of bots
+		for element in self.swarm.botList:
+			center = QPoint(element.xPos, element.yPos)
+			paint.drawEllipse(center, element.size, element.size)
+
+		# self.bot.calcVelocity()
+		# self.bot.calcPos()
+
+		self.moveToMouse()
+
+		paint.end()
+
+
+
+
+#-----------------------------------------------------------#
+def run():
+	app = QtGui.QApplication(sys.argv)
+	GUI = Window()
+	sys.exit(app.exec_())
+
+
+#-----------------------------------------------------------#
+if __name__ == "__main__":
+	run()
